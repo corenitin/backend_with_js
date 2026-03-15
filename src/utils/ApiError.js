b308a2d9830628eded3328d48d39447c33258b23
corenitin <parmarnitin1306@gmail.com>
2026-03-15 14:40:12 +0530

    add user and video model

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -5,12 +5,13 @@ class ApiError extends Error {
     errors = [],
     stack = ""
   ) {
-    (super(message),
-      (this.statusCode = statusCode),
-      (this.data = null),
-      (this.message = message),
-      (this.success = false),
-      (this.errors = errors));
+    super(message)
+
+    this.statusCode = statusCode
+    this.data = null
+    this.message = message
+    this.success = false
+    this.errors = errors
 
     if (stack) {
       this.stack = stack;
